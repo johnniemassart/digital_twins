f3edeb92c2e46030e8aee0cab54a01c291199aba
Johnnie Massart <johnniemassart@Johnnies-MacBook-Pro.local>
2024-06-23 22:34:14 -0500

    a basis

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,17 +8,29 @@ import {
   Stars,
   Environment,
   ContactShadows,
+  PerspectiveCamera,
 } from "@react-three/drei";
+import { DoubleSide } from "three";
 
-const MoonSphere = () => {
+const MoonSphere = ({ size }) => {
   const texture = useTexture("../src/assets/moonmap1k.jpg");
   return (
-    <mesh position={[1, 0, 0]}>
+    <mesh position={[0, 0, 0]}>
       <ambientLight intensity={1} />
       <directionalLight />
       <Environment preset="night" />
-      <sphereGeometry />
-      <meshStandardMaterial map={texture} displacementScale={0.2} />
+      <sphereGeometry args={[size, 32, 32]} />
+      <meshPhongMaterial map={texture} displacementScale={0.2} />
+    </mesh>
+  );
+};
+
+const Plane = ({ position, rotation, args }) => {
+  return (
+    <mesh position={position} rotation={rotation}>
+      <ambientLight intensity={1} />
+      <planeGeometry args={args} />
+      <meshBasicMaterial color="#1a1a1a" side={DoubleSide} />
     </mesh>
   );
 };
@@ -26,11 +38,29 @@ const MoonSphere = () => {
 const Home = () => {
   return (
     <>
-      <HomeToLogin />
+      {/* <HomeToLogin /> */}
       <Canvas id="canvas">
-        <OrbitControls autoRotate autoRotateSpeed={0.5} enableZoom={false} />
-        <Stars radius={250} />
-        <MoonSphere />
+        <Suspense>
+          <PerspectiveCamera position={[3, 3, 3]} makeDefault />
+          <OrbitControls autoRotate autoRotateSpeed={0.3} />
+          <Stars radius={250} />
+          <MoonSphere size={0.5} />
+          <Plane
+            position={[0, -0.5, 0]}
+            rotation={[Math.PI / 2, 0, 0]}
+            args={[2, 2]}
+          />
+          <Plane
+            position={[0, 0, -1]}
+            rotation={[Math.PI, 0, 0]}
+            args={[2, 1]}
+          />
+          <Plane
+            position={[-1, 0, 0]}
+            rotation={[Math.PI, Math.PI / 2, 0]}
+            args={[2, 1]}
+          />
+        </Suspense>
       </Canvas>
     </>
   );
